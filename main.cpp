eee93b323e603476aaa4f3c79d66379c94c7dd32
Akil0814 <akil040814@gmail.com>
2026-02-04 17:33:05 -0800

    testing sqlite

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include "home_page.h"
+#include "data_sqLite.cpp"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QStandardPaths>
+#include <QDir>
+#include <QDebug>
 
 #include <QApplication>
 
@@ -7,7 +14,27 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     HomePage window;
     window.show();
+
+    //test sqlite
+    if (!initSqlite("main")) return 1;
+
+    // demo
+    {
+        QSqlDatabase db = QSqlDatabase::database("main");
+        QSqlQuery q(db);
+        q.prepare("INSERT INTO t(name) VALUES(?);");
+        q.addBindValue("hello");
+        if (!q.exec()) {
+            qDebug() << "INSERT failed:" << q.lastError().text();
+            return 1;
+        }
+    }
+
+    //test sqlite
+
+    qDebug() << "ok";
     a.exec();
+
 
     return 0;
 }
