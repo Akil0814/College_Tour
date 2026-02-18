4e068b3f1cc1a654bf06bf16f0b7e670c029df81
Zachary Ollivierre <zollivierre0@saddleback.edu>
2026-02-18 13:06:38 -0800

    Updated path_algo naming conventions and comments

--- path_algo.cpp
+++ path_algo.cpp
@@ -6,60 +6,57 @@
 This function recursively visits the nearest unvisited campus by linearly searching remaining campuses to visit,
 choosing the closest one from the current stop, removing it from remaining, and recursing until no campuses remain.
 
-If no edges exist between current campus and all remaining a runtime error is thrown.
+If no edges exist between current campus and all remaining a runtime error is thrown. Otherwise
+visit order will store college ids in the order to be visited.
 */
 
 void visit_next(
     int current_college_id,
-    QVector<int> remaining_colleges,
-    QVector<int> visit_order,
+    QVector<int> &remaining_colleges,
+    QVector<int> &visit_order,
     double &total_distance,
     const DataManager &data_manager)
 {
-    // Base case if there are no more remaining colleges to visit
+    // Base case | there are no more remaining colleges to visit
     if (remaining_colleges.isEmpty())
     {
         return;
     }
 
-    //
-    int closest_id = -1;
-    int closest_index = -1;
+    int nearest_campus_id = -1;
+    int nearest_campus_index = -1;
 
-    // Init min distance to largest int so all valid distances are smaller
-    int min_distance = std::numeric_limits<int>::max();
+    // Ensures first valid distance will always be set to the min distance
+    int nearest_campus_dist = std::numeric_limits<int>::max();
 
-    // For all remaining unviseted colleges
+    // Search through unvisited campuss
     for (int i = 0; i < remaining_colleges.size(); i++)
     {
-        int next_id = remaining_colleges[i];
+        int next_campus_id = remaining_colleges[i];
 
-        // Get distance between current college and next canidate | nullptr if not found
-        auto distance = data_manager.get_distance_between_college(current_college_id, next_id);
+        // Get distance between current college and next canidate | nullptr if no valid edge
+        auto distance = data_manager.get_distance_between_college(current_college_id, next_campus_id);
 
-        // Check if distance is valid and check if its the current smallest
-        if (distance && *distance < min_distance)
+        // Check if distance is valid and check if its the current smallest | always true on first iteration
+        if (distance != nullptr && *distance < nearest_campus_dist)
         {
-            // Update new shortest
-            min_distance = *distance;
-
-            // Store id and store index for removal
-            closest_id = next_id;
-            closest_index = i;
+            // Update new shortest distance and record campus id and location in remaining
+            nearest_campus_dist = *distance;
+            nearest_campus_id = next_campus_id;
+            nearest_campus_index = i;
         }
     }
 
-    // Check that a next stop is found
-    if (closest_id == -1)
+    // Throw error if next stop is not found | nearest is still sentinal
+    if (nearest_campus_id == -1)
     {
         throw std::runtime_error("No reachable college was found from current campus id:" + std::to_string(current_college_id));
     }
 
     // Update state | total distance | visit order | remove from remaining
-    total_distance += min_distance;
-    visit_order.push_back(closest_id);
-    remaining_colleges.removeAt(closest_index);
+    total_distance += nearest_campus_dist;
+    visit_order.push_back(nearest_campus_id);
+    remaining_colleges.removeAt(nearest_campus_index);
 
-    // Recurse
-    visit_next(closest_id, remaining_colleges, visit_order, total_distance, data_manager);
+    visit_next(nearest_campus_id, remaining_colleges, visit_order, total_distance, data_manager);
 }
