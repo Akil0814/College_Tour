afff292b2144e6cba897351514d49a4ce0cb1d4c
Akil0814 <akil040814@gmail.com>
2026-02-20 23:22:17 -0800

    update manger

--- manager.h
+++ manager.h
@@ -1,33 +1,6 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
-// Generic singleton helper: gives you one shared instance of T.
-/*
-    Manager<T> is a generic "singleton helper" base class.
-
-    Goal:
-    - Make exactly ONE shared instance of some class T
-    - Let everyone access it through T::instance()
-
-    How it's meant to be used:
-    class SomethingManager : public Manager<SomethingManager>
-    {
-        friend class Manager<SomethingManager>; // allows Manager to call SomethingManager's constructor
-    protected:
-        SomethingManager();
-        ~SomethingManager();
-    public:
-        void SomeFunc()
-        .....
-    };
-
-    Then:
-        auto manager_ptr=SomethingManager::instance();
-        manager_ptr->SomeFunc();
-     or:
-        SomethingManager::instance()->SomeFunc();
-*/
-
 template<typename T>
 class Manager
 {
