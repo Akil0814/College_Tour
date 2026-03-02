7435977686ae4d79f128b6d4298a89b9fd68a9d2
Zachary Ollivierre <zollivierre0@saddleback.edu>
2026-03-01 19:47:30 -0800

    Finished all functionality of admin page

--- adminpage.h
+++ adminpage.h
@@ -15,8 +15,28 @@ public:
     explicit adminpage(QWidget *parent = nullptr);
     ~adminpage();
 
+private slots:
+    void on_editTypeDD_currentIndexChanged(int index);
+
+    void on_schoolSelectDD_currentIndexChanged(int index);
+
+    void on_itemDD_currentIndexChanged(int index);
+
+    void on_removeButton_clicked();
+
+    void on_saveChangesButton_clicked();
+
+    void on_editInputFileButton_clicked();
+
+    void on_resetButton_clicked();
+
+    void on_removeSchoolButton_clicked();
+
 private:
     Ui::adminpage *ui;
+    void populateColleges();
+    void populateEditTypes();
+    std::optional<double> check_price(QString price_text);
 };
 
 #endif // ADMINPAGE_H
